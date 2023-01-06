fbcd41dd06d5f6ed5f635654aa4d503e48cd01ae
Matthew Cockram <matrat5114@hotmail.com>
2023-01-06 15:42:05 +1030

    Add project files.

---
+++ Pets_R_Us/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pets_R_Us.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
