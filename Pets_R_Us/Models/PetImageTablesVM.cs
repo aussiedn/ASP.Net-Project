d74619dee4ab53219f7209123c5204a02ecd87a1
Matthew Cockram <matrat5114@hotmail.com>
2023-02-01 14:37:36 +1030

    Add uploading images

--- Pets_R_Us/Models/PetImageTablesVM.cs
+++ Pets_R_Us/Models/PetImageTablesVM.cs
@@ -17,5 +17,12 @@ namespace Pets_R_Us.Models
         [Display(Name = "Image caption")]
         [Required]
         public string ImageCaption { get; set; }
+
+        [Display(Name = "Upload your image")]
+        [Required]
+        public IFormFile PetImage { get; set; }
+
+        [Display(Name = "Your pet photos")]
+        public string? PetImageUrl { get; set; }
     }
 }
